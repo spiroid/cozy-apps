55f1a33415be29480f838a046bff84adefb6cd59
Rony Dray <contact@obigroup.fr>
2015-12-24 19:39:13 +0100

    Chown cozy-data-system user on /usr/local/var/cozy/data-system

--- init.sh
+++ init.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
 while ! curl -s $COUCH_HOST:$COUCH_PORT; do sleep 5 && echo "waiting for couchdb"; done
-# Waiting for indexer to be available
-while ! curl -s $INDEXER_HOST:$INDEXER_PORT; do sleep 5 && echo "waiting for data indexer" ; done
 
 # Waiting for local controller to be up and running
 while ! curl -s 127.0.0.1:9002; do sleep 5 && echo "waiting for controller"; done
@@ -10,8 +7,8 @@ while ! curl -s 127.0.0.1:9002; do sleep 5 && echo "waiting for controller"; don
 (cozy-monitor status || exit 3)
 if [ $? = 3 ]; then
     chown -hR cozy /etc/cozy
-    chown -R cozy:cozy /usr/local/cozy-indexer
     chown cozy-data-system /etc/cozy/couchdb.login
+    chown -R cozy-data-system:cozy-data-system /usr/local/var/cozy/data-system
     chmod 640 /etc/cozy/couchdb.login
 
     cozy-monitor install data-system
